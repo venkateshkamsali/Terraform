f4919df62dd25c2e15d58193cc4b1df58dadadb2
venkateshkamsali <kvenkatesh4595@gmail.com>
2026-01-12 13:00:42 +0530

    Updated variable file

--- variable.tf
+++ variable.tf
@@ -3,3 +3,13 @@ variable "instance_type" {
   description = "EC2 instance type for the web server"
   default     = "t2.micro"
 }
+variable "key_name" {
+  default = "Kidvento.pem" 
+}
+variable "ami" {
+  default = "ami-02b8269d5e85954ef" 
+}
+variable "region" {
+  default = "ap-south-1"
+  
+}
